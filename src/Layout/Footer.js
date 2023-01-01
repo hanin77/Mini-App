2fc4b4fedd04793485b21333b3dccc614c923e61
hanin77 <mohammadhanin77@gmail.com>
2023-01-01 20:21:04 +0100

    feat: add basic Footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,7 +1,13 @@
-import React from "react"
-
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer">
+      <span className="d-block d-md-inline-block">
+        <span className="d-sm-inline-block text-secodary">
+          {"All Rights Reserved"} © {new Date().getFullYear()}{" "}
+        </span>
+      </span>
+    </footer>
+  )
 }
 
 export default Footer
