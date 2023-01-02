5267f70dd7f84ba972dc49c17e311649711f5011
hanin77 <mohammadhanin77@gmail.com>
2023-01-02 17:10:28 +0100

    feat: implement users page with data fetching and userCards

--- src/views/pages/Users/index.js
+++ src/views/pages/Users/index.js
@@ -1,8 +1,43 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+import { Row, Col, Container, Spinner } from "reactstrap"
+import { getUsers } from "../../../services"
+import UserCard from "./UserCard"
+
 const Users = () => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    getUsers()
+      .then((res) => {
+        setData(res?.data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+      })
+  }, [])
   return (
-    <>
-      <h2 className="text-center text-primary">Users List</h2>
-    </>
+    <Container>
+      <Row>
+        <Col lg="5" md="12">
+          {data.length === 0 && !loading ? (
+            <h4 className="text-info">There are no Users to display</h4>
+          ) : (
+            <h4 className="text-center text-secondary">Users List</h4>
+          )}
+        </Col>
+        <Col className="users-list" lg="7" md="12">
+          {loading && (
+            <Row className="justify-content-center mt-1">
+              <Spinner type="border" color="secondary" children={false} />
+            </Row>
+          )}
+          {!!data.length &&
+            data.map((user) => <UserCard key={user.email} user={user} />)}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
