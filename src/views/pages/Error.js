a078d2d0fabfc23cbcf6b7befd643d2c806c6ff7
hanin77 <mohammadhanin77@gmail.com>
2023-01-02 23:23:13 +0100

    feat: add Error page final version

--- src/views/pages/Error.js
+++ src/views/pages/Error.js
@@ -1,9 +1,32 @@
-import React from "react"
+import { Link } from "react-router-dom"
+import { Container, Col } from "reactstrap"
 const Error = () => {
   return (
-    <div className="d-flex flex-column align-items-center">
-      <p className="display-4">Page not Found</p>
-    </div>
+    <Container>
+      <Col className="text-center">
+        <p className="display-1">404</p>
+      </Col>
+      <Col className="text-center">
+        {" "}
+        <p className="display-4">Page not Found</p>
+      </Col>
+      <Col className="text-center">
+        {" "}
+        <p className="text-info">
+          {" "}
+          Go
+          <span>
+            <Link
+              className="font-weight-bold text-info text-decoration-none"
+              to="/users"
+            >
+              {" "}
+              Home
+            </Link>
+          </span>
+        </p>
+      </Col>
+    </Container>
   )
 }
 
